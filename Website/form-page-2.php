fc902ad311eb53367b4e021300d6b1bba490a66e
danielsimmen <165002010+danielsimme@users.noreply.github.com>
2024-05-27 10:35:25 +0200

    forms and in css my styles

--- Website/form-page-2.php
+++ Website/form-page-2.php
@@ -139,8 +139,8 @@
         <h1>Contact Us</h1>
 
         <ul class="compass">
-            <li class="compassactive">1</li>
-            <li class="compassitem">2</li>
+            <li class="compassitem">1</li>
+            <li class="compassactive">2</li>
             <li class="compassitem">3</li>
         </ul>
 
@@ -148,58 +148,28 @@
         <form id="contactForm">
 
             <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" id="name" name="name" required>
+                <label for="email">E-Mail</label>
+                <input type="email" id="email" name="email" placeholder="name@email.com" required>
             </div>
+
 
             <div class="form-group">
-                <label for="surname">Surname</label>
-                <input type="text" id="surname" name="surname" required>
-            </div>
-
-            <div class="form-group">
-                <label for="company">Company</label>
-                <input type="text" id="company" name="company">
                 <div class="form-group">
-                    <label for="email">E-Mail</label>
-                    <input type="email" id="email" name="email" placeholder="name@email.com" required>
+                    <label for="phone">Phone Number (Swiss):</label><br>
+                    <input type="tel" id="phone" placeholder="041 079 123 45 67" name="phone" pattern="[0-9]{3} [0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}">
                 </div>
 
-
-                <div class="form-group">
-                    <div class="form-group">
-                        <label for="phone">Phone Number (Swiss):</label><br>
-                        <input type="tel" id="phone" placeholder="041 079 123 45 67" name="phone" pattern="[0-9]{3} [0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}">
-                    </div>
-
-                    <script>
-                        // Function to automatically add spaces to the phone number input
-                        document.getElementById('phone').addEventListener('input', function(e) {
-                            let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
-                            e.target.value = !x[2] ? x[1] : x[1] + ' ' + x[2] + (x[3] ? ' ' + x[3] + (x[4] ? ' ' + x[4] + (x[5] ? ' ' + x[5] : '') : '') : '') + (x[6] ? ' ' + x[6] + (x[7] ? ' ' + x[7] : '') : '');
-                        });
-                    </script>
-
-                </div>
-
-                <div class="form-group">
-                    <label for="options">Regarding</label>
-                    <select id="options" name="options" required>
-                        <option value="">Select Option</option>
-                        <option value="option1">Job Offer</option>
-                        <option value="option2">Question Regarding Assets</option>
-                        <option value="option3">Other</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="message">Your Message</label>
-                    <textarea id="message" name="message" required></textarea>
-                </div>
+                <script>
+                    // Function to automatically add spaces to the phone number input
+                    document.getElementById('phone').addEventListener('input', function(e) {
+                        let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
+                        e.target.value = !x[2] ? x[1] : x[1] + ' ' + x[2] + (x[3] ? ' ' + x[3] + (x[4] ? ' ' + x[4] + (x[5] ? ' ' + x[5] : '') : '') : '') + (x[6] ? ' ' + x[6] + (x[7] ? ' ' + x[7] : '') : '');
+                    });
+                </script>
 
             </div>
 
-            <button type="submit">Submit</button>
+            <button type="submit">Next</button>
 
         </form>
     </div>
